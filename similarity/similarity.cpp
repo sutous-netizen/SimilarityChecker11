f077c7beec29c5483bd4880d5b0c629f77c2d45e
test <test@test.com>
2026-07-01 17:05:52 +0900

    [refactoring] comp method refact

--- similarity/similarity.cpp
+++ similarity/similarity.cpp
@@ -6,16 +6,21 @@ class LengthChecker {
 public:
 	int comp(string inStr1, string inStr2) {
 		double result = 0;
-		if (inStr1.length() >= inStr2.length() * 2
-			|| inStr1.length() * 2 <= inStr2.length()) {
-			return result;
+		length1 = inStr1.length();
+		length2 = inStr2.length();
+		if (length1 >= length2 * 2
+			|| length1 * 2 <= length2) {
+			return static_cast<int>(result);
 		}
 		else {
-			double gap = inStr1.length() >= inStr2.length() ? inStr1.length() - inStr2.length() : inStr2.length() - inStr1.length();
-			double minLenth = inStr1.length() >= inStr2.length() ? inStr2.length(): inStr1.length();
+			double gap = length1 >= length2 ? length1 - length2 : length2 - length1;
+			double minLenth = length1 >= length2 ? length2 : length1;
 			result = (1 - gap / minLenth) * 60;
 			return static_cast<int>(result);
 		}
 		//return 60;
 	}
+private:
+	int length1;
+	int length2;
 };  
