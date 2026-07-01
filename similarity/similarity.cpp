3db1845a2cb2859bd4cd3d5ee02da7be094d6b80
test <test@test.com>
2026-07-01 16:33:42 +0900

    [feature] towice check

--- similarity/similarity.cpp
+++ similarity/similarity.cpp
@@ -5,6 +5,10 @@ using std::string;
 class LengthChecker {
 public:
 	int comp(string inStr1, string inStr2) {
+		if (inStr1.length() >= inStr2.length() * 2
+			|| inStr1.length() * 2 <= inStr2.length()) {
+			return 0;
+		}
 		return 60;
 	}
 };
--- similarity/similarity_test.cpp
+++ similarity/similarity_test.cpp
@@ -4,10 +4,21 @@
 
 using std::string;
 
+
+
+
 TEST(Length, split) {
 	LengthChecker lengthChecker;
 	string inStr1 = "ASD", inStr2 = "DSA";
 	int expect = 60;
+	int actual = lengthChecker.comp(inStr1, inStr2);
+
+	EXPECT_EQ(expect, actual);
+}
+TEST(Length, split2) {
+	LengthChecker lengthChecker;
+	string inStr1 = "A", inStr2 = "BB";
+	int expect = 0;
 	int actual = lengthChecker.comp(inStr1, inStr2);
 
 	EXPECT_EQ(expect, actual);
